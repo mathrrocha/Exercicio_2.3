e22e43374c03ba2edce30a47b8bd860049cf1ce0
mathrrocha <matheusrochareis@gmail.com>
2019-04-02 10:11:37 -0300

    Versão inicial

---
+++ Exercicio 2.3/Exercicio 2.3/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Exercicio_2._3
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
